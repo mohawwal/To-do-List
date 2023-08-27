71716167e52046a031b6f42a47e17d263f383745
Anileleye Awwal <aanileleye@gmail.com>
2023-08-27 11:54:50 +0100

    added a new method

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
-const addBtn = document.querySelector(".push");
-const taskInputBox = document.querySelector(".addTaskInputBox")
-const taskSpace = document.querySelector(".tasks-container")
-const WarningMsg = document.querySelector(".warningText")
+// const addButton = document.getElementById("addButton");
+// const inputText = document.getElementById("inputText");
+// const itemList = document.getElementById("itemList");
+// const itemWarning = document.getElementById("itemWarning");
+// const deleteText = document.getElementById("deleteText");
 
 
+// addButton.addEventListener('click', () => {
+    
+//     if(inputText.value == "") {
+//         itemWarning.textContent = "Input an item"
+//     }else {
+//         const listDiv = document.createElement("div")
+//         listDiv.innerHTML = `
+//             <div class="eachList">${inputText.value}
+//                 <h3 class="deleteItem">DEL</h3>
+//             </div>
+//         `
+//         itemList.appendChild(listDiv)
+//         inputText.value = "";
+        
+//         // const deleteAText = document.querySelector(".deleteAText")
+//         // deleteAText.addEventListener((click), ()=> {
+//         //     
+//         // })
 
-addBtn.addEventListener("click", () => {
-    if (taskInputBox.value.length == 0) {
-        return WarningMsg.innerHTML= "Enter a task"
-    } else {
-        taskSpace.innerHTML +=`<div class="tasks">
-                                    <span class="taskInput">
-                                        ${taskInputBox.value}
-                                    </span>
-                                    <button class="delete">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
-                                </div>
-                            `;
-        taskInputBox.value = "";
-        const delBtn = document.querySelectorAll(".delete")
-        for (let i =0; i<delBtn.length; i++) {
-            delBtn[i].addEventListener("click", function() {
-                this.parentNode.remove();
-            })
-        }
+//         const eachList = document.querySelectorAll(".eachList")
+//         const deleteItem = document.querySelectorAll(".deleteItem")
+
+//         deleteItem.forEach((items, i) => {
+//             items.addEventListener('click',()=> {
+//                 eachList[i].remove()
+//             })
+//         })
+        
+//     }
+// })
+
+////SECOND METHOD USING A FUNCTION
+
+// Get DOM elements
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+
+//Add a function for new tasks
+
+function addTask() {
+    const taskText =taskInput.value;
+
+    if (taskText.trim() !== "") {
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = `<span>${taskText}</span><button class="deleteButton">Delete</button>`;
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+    }
+}
+
+addButton.addEventListener('click', addTask)
+
+taskList.addEventListener('click', function(event) {
+    if(event.target.classList.contains("deleteButton")) {
+        event.target.parentElement.remove()
     }
 })
